2a0152f31a6ebd024774d6a4654f166facbce987
vauxden <vauxden@outlook.com>
2019-12-19 22:49:51 -0500

    Final Push

--- jingle/app/Http/Controllers/CommentController.php
+++ jingle/app/Http/Controllers/CommentController.php
@@ -3,84 +3,32 @@
 namespace App\Http\Controllers;
 use App\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $comments = Comment::all();
         return view('comments.index', ['comments' => $comments]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function apiStore(Request $request)
     {
-        //
+       $e = new Comment;
+       $e->comment = $request['comment'];
+       $e->likes = 0;
+       $e->user_id = $request['user_id'];
+       $e->post_id = $request['post_id'];
+
+       $e->save();
+       return $e;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
       $comment = Comment::findOrFail($id);
       return view('comments.show', ['comment' => $comment]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
